00e31b6c21b1ad3323e30a72535de94551a092c0
Ben Hummon <ben.hummon@gmail.com>
2021-06-12 14:31:11 -0500

    WIP

--- test/services/words.test.js
+++ test/services/words.test.js
@@ -1,10 +1,69 @@
 const assert = require('assert');
 const app = require('../../src/app');
+const { buildUppercaseAlphabet } = require('../../src/utilities/alphabet');
 
 describe('\'words\' service', () => {
   it('registered the service', () => {
     const service = app.service('words');
-
     assert.ok(service, 'Registered the service');
+  });
+
+  it('finds words of length two', async () => {
+    const service = app.service('words');
+    const results = await service.find({ length:  2 });
+    assert.equal(
+      results.length,
+      427,
+      'There are many words of length two'
+    );
+  });
+
+  it('finds words of length one', async () => {
+    const service = app.service('words');
+    const results = await service.find({ length: 1 });
+    assert.equal(
+      results.length,
+      26,
+      'Returns twenty six words of length one'
+    );
+    assert.deepEqual(
+      results,
+      buildUppercaseAlphabet(),
+      'The words of length one are just the letters of the alphabet'
+    );
+  });
+
+  it('finds words when we include a regular expression', async () => {
+    const service = app.service('words');
+    const results = await service.find({ length: 3, regExp: /^C.D?$/ });
+    assert.equal(
+      results.length,
+      7,
+      'There are many words of length three matching the regular expression'
+    );
+    assert.ok(
+      results.includes('COD'),
+      'The word COD is included in the results'
+    );
+  });
+
+  it('finds no words of length zero', async () => {
+    const service = app.service('words');
+    const results = await service.find({ length: 0 });
+    assert.equal(
+      results.length,
+      0,
+      'There are no words of length zero'
+    );
+  });
+
+  it('finds no words when length is not specified correctly', async () => {
+    const service = app.service('words');
+    const results = await service.find({ length: 'a' });
+    assert.equal(
+      results.length,
+      0,
+      'Found no words'
+    );
   });
 });
