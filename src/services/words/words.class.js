310e83d6d497788f84cd0e2d3931008eb8dced98
Ben Hummon <ben.hummon@gmail.com>
2021-06-12 09:13:14 -0500

    cleaning up words class

--- src/services/words/words.class.js
+++ src/services/words/words.class.js
@@ -1,43 +1,66 @@
 /* eslint-disable no-unused-vars */
 
 const fs = require('fs');
+const { inclusiveIndicesArray } = require('../../utilities/arrays');
+
+const minWordLength = 1;
+const maxWordLength = 10;
 
 exports.Words = class Words {
   constructor (options, app) {
     this.app = app;
-    this.words_lists = [];
-    this.words_lists[0] = [];
-    for (let i = 1; i <= 10; i++) {
-      this.words_lists[i] = this._knownWordsWithLength(i);
-    }
   }
 
   async find(params) {
-    const { length, regExp } = params;
-    if (! this._isValidLength(length)) return [];
-    const words = this.words_lists[length];
-    if (! regExp) return words;
-    return words.filter(
-      word => regExp.test(word)
-    );
+    try {
+      const { length, regExp } = params;
+      if (! this._isValidLength(length)) return [];
+      await this._ensureWordsLoaded();
+      const words = this.words_lists[length];
+      if (! regExp) return words;
+      return words.filter(
+        word => regExp.test(word)
+      );
+    } catch (error) {
+      console.error('Error occurred in find method of words service:', error);
+    }
   }
 
   _isValidLength(length) {
     if (typeof length !== 'number') return false;
-    if (length < 1 || length > 10) return false;
+    if (length < minWordLength || length > maxWordLength) return false;
     return true;
   }
 
-  _knownWordsWithLength(length) {
+  _ensureWordsLoaded() {
+    if (this._loadingWordsPromise) return this._loadingWordsPromise;
+    this._loadingWordsPromise = this._loadWords();
+    return this._loadingWordsPromise;
+  }
+
+  async _loadWords() {
     try {
-      const fileName = `word_lists/words${length}.txt`;
+      this.words_lists = [];
+      await Promise.all(
+        inclusiveIndicesArray(minWordLength, maxWordLength).map(
+          i => this._loadWordsOfLength(i)
+        )
+      );
+    } catch (error) {
+      console.error('Error occurred loading words:', error);
+    }
+  }
+
+  async _loadWordsOfLength(i) {
+    try {
+      const fileName = `word_lists/words${i}.txt`;
       const fileAsString = fs.readFileSync(fileName, 'utf8');
       const words = fileAsString.split('\n');
-      return words.filter(
+      this.words_lists[i] = words.filter(
         word => /[A-Z]+/.test(word)
       );
-    } catch (err) {
-      console.error(err);
+    } catch (error) {
+      console.error(`Error occurred loading words of length ${i}:`, error);
       return [];
     }
   }
